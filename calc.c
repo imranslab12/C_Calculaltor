8e7415ab16bc0b5a23ea8144347db63cd74ab8f1
imranslab12 <imrananwar3124@gmail.com>
2025-06-07 01:31:51 +0100

    Update calc.c
    2nd commit, and major corrections to code including types, operators and syntax

--- calc.c
+++ calc.c
@@ -3,47 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+int main(){
+
 char operator;
 double num1;
 double num2;
 double result;
 
-
-
-
 printf("\n what operator  + - * /" );
-scanf("%c", &operator);
+scanf("%lf", &operator);
 
 printf("\n enter num 1" );
-scanf("%c", &num1);
+scanf("%lf", &num1);
 
 printf("\n enter num 2" );
-scanf("%c", &num2);
+scanf("%lf", &num2);
 
 
 
-double calculateVal(num1, num2){
-    scanf("%d", &operator)
-    switch(operator)
+
+    switch(operator){
     case '+':
     result = num1 + num2;
-    printf("\n result");
-
-    scanf("%d", &operator)
-    switch(operator)
+    printf("\n result", result);
+    break;
     case '-':
     result = num1 - num2;
-    printf("\n result");  
-
-    scanf("%d", &operator)
-    switch(operator)
+    printf("\n result", result);  
+    break;
     case '*':
     result = num1 * num2;
-    printf("\n result");  
-
-    scanf("%d", &operator)
-    switch(operator)
+    printf("\n result", result);  
+    break;
     case '/':
     result = num1 / num2;
-    printf("\n result");    
+    printf("\n result", result);
+    break;    
+    default:
+      printf("%c is not valid", operator);
+
+    }
+   return 0;
 }
