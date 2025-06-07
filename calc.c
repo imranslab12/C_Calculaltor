198a6e0040b1e9e9e71514e2bc2d58ec1b3aa0a9
imranslab12 <imrananwar3124@gmail.com>
2025-06-07 01:14:22 +0100

    Update calc.c
    initial ideal code for the calculator

--- calc.c
+++ calc.c
@@ -1,3 +1,49 @@
-int main(){
-    int i = 0;
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char operator;
+double num1;
+double num2;
+double result;
+
+
+
+
+printf("\n what operator  + - * /" );
+scanf("%c", &operator);
+
+printf("\n enter num 1" );
+scanf("%c", &num1);
+
+printf("\n enter num 2" );
+scanf("%c", &num2);
+
+
+
+double calculateVal(num1, num2){
+    scanf("%d", &operator)
+    switch(operator)
+    case '+':
+    result = num1 + num2;
+    printf("\n result");
+
+    scanf("%d", &operator)
+    switch(operator)
+    case '-':
+    result = num1 - num2;
+    printf("\n result");  
+
+    scanf("%d", &operator)
+    switch(operator)
+    case '*':
+    result = num1 * num2;
+    printf("\n result");  
+
+    scanf("%d", &operator)
+    switch(operator)
+    case '/':
+    result = num1 / num2;
+    printf("\n result");    
 }
